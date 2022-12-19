919eb40f10e4080fae2b8fbd3b9fa899241f0e4d
bel4enok03 <belskaya-tanyush@mail.ru>
2022-12-20 00:58:09 +0400

    *

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { data } from './data';
 import './App.css';
 
+
 function App() {
+  const [movie, setMovie] = useState(0);
+  const {id, name, genre, year, image } = data[movie];
+  const previousMovie = () => { 
+    setMovie ((movie => {
+      movie --;
+      if (movie < 0) {
+        return data.length - 1;
+      }
+      return movie;
+    }))
+  }
+
+  const nextMovie = () => {
+    setMovie ((movie => {
+      movie ++;
+      if (movie > data.length - 1) {
+        movie = 0;
+      }
+      return movie;
+    }))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='container'>
+        <h1>Viewing list of {data.length} movies</h1>
+      </div>
+      <div className='container'>
+        <img src={image} width='300px' height='450px' alt='movie' />
+      </div>
+      <div className='container'>
+      <h2>{id} - {name}</h2>
+      </div>
+      <div className='container'>
+        <p>{genre}</p>
+      </div>
+      <div className='container'>
+      <p>{year}</p>
+      </div>
+    <div className='btn container'>
+      <button onClick={previousMovie}>Previous</button>
+      <button onClick={nextMovie}>Next</button>
+    </div>
     </div>
   );
 }
---
+++ src/data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        id:1,
+        name:'The Crown',
+        genre: 'Drama - Historical',
+        year: 2011,
+        image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/5e862b4d-9db8-45b9-ad19-3b3ebb410bd5/3840x'
+    },
+    {
+        id:2,
+        name:'Red Lights',
+        genre: 'Drama - Fantasy - Mystery',
+        year: 2016,
+        image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1898899/1f7b1540-3085-46a8-8d4a-bd7b2849f189/3840x'
+    },
+    {
+        id:3,
+        name:'Spotlight',
+        genre: 'Biography - Crime - Drama',
+        year: 2015,
+        image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1773646/358e579c-9b76-44c6-a754-5235897ab246/3840x'
+    },
+    {
+        id:4,
+        name:'This Is Us',
+        genre: 'Comedy - Drama',
+        year: 2016,
+        image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1900788/228de9d1-fdaf-4fe2-97e6-e5e6edd7b595/3840x'
+    },
+    {
+        id:5,
+        name:'The Peripheral',
+        genre: 'Mystery - Drama',
+        year: 2022,
+        image: 'https://avatars.mds.yandex.net/get-kinopoisk-image/6201401/b7657863-de36-4664-8875-8346ba483ecc/3840x'
+    },
+]
